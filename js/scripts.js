29d8d046771a64d962abf317207cd86e66cb84fd
adriviris <adrianacarrenog@gmail.com>
2022-11-29 19:44:14 -0500

    Pokemon list
    Contains list of arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
-alert('Hello world');
 
+let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=500';
 
-let favoriteFood = 'Pizza'
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Lucario', height: 4, types: ['steel', 'speed']}
+    {name: 'Vaporeon', height: 3, types: 'water'}
+    {name: 'Snorlax', height: 7, types: ['fat', 'immunity']}
+    {name: 'Mewtwo', height: 6, types: 'pressure'}
+    {name: 'Ninetales', height: 3, types: 'fire'}
+]
