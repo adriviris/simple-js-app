b2847bd4161780e22ec3a1081871a96bbe2ec27f
adriviris <adrianacarrenog@gmail.com>
2022-12-04 11:17:40 -0500

    Added Loops

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,14 @@ let pokemonList = [
     {name: 'Mewtwo', height: 6, types: 'pressure'}
     {name: 'Ninetales', height: 3, types: 'fire'}
 ];
+
+for (let i = 0;  
+    i < pokemonList.length; i++) {
+        if (pokemonList [i].height > 3) {
+            document.write(pokemonList[i].name + " is " + pokemonList[i].height + " feet tall and it is a " + pokemonList[i].types + " type! Wow! It is really tall")
+        }
+        else {
+            document.write (pokemonList[i].name + " is " + pokemonList[i].height) + "feet tall and it is a " + pokemonList[i].types + "type!"
+        }
+    
+}
